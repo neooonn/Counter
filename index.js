3052aeb0733b18abc2ae46c52ea374390184913f
neooonn <124420877+neooonn@users.noreply.github.com>
2024-07-31 05:19:36 +0800

    Add Counter files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.textContent = count;
+}
